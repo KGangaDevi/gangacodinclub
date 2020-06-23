b87f8629120b92ede65e075f536a57ba6be54407
gangadevi <kakarlagangadevi@gmail.com>
2020-06-23 12:26:18 +0530

    First commit

---
+++ Helloworld.java
@@ -0,0 +1,18 @@
+public class Helloworld
+{
+	public static void main(String args[])
+	{
+		while(true)
+		{
+			System.out.println("Welcome");
+			try
+			{
+			Thread.sleep(60000);
+			}
+			catch(Exception ex)
+			{
+				System.out.println(ex);
+			}
+		}
+	}
+}
